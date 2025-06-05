0fd9391850249f18e6876dccf5567a5659cfa738
FK <felix.korbendau@free.fr>
2025-06-05 12:39:50 +0200

    average rating OK

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :client
   belongs_to :artist
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
 
-  validates
+  validates :comment, presence: true
+  validates :rating, presence: true, numericality: { in: 1..5 }
 end
