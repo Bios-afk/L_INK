4a11f1de8d2ee8ffb1a297d0becf778d90703174
FK <felix.korbendau@free.fr>
2025-06-08 21:43:04 +0200

    css ok pour le positionnement des sections

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,13 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.where(message_feed_id: params[:message_feed_id]).order(created_at: :asc)
     @feed = MessageFeed.find(params[:message_feed_id])
-    
+
     @artist = @feed.artist
     @client = @feed.client
 
     @message = Message.new
+
+    @hide_navbar = true
 
     if current_user.userable_type == "Client"
       @user = @feed.artist
--- app/javascript/controllers/autosize_controller.js
+++ app/javascript/controllers/autosize_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="autosize-input-message"
 export default class extends Controller {
   connect() {
+    console.log('controller resize ok')
     this.resize()
     this.element.addEventListener("input", () => this.resize())
   }
