abe7e9a7b4d9af855fca22ee887adf6b09e94b29
Hadrien Senegas <hadrien.senegas@gmail.com>
2025-06-09 12:35:17 +0200

    form ok

--- app/controllers/quote_requests_controller.rb
+++ app/controllers/quote_requests_controller.rb
@@ -9,8 +9,14 @@ class QuoteRequestsController < ApplicationController
   end
 
   def create
+    artist = Artist.find_by(id: quote_request_params[:artist_id])
+    unless artist
+      redirect_back fallback_location: root_path, alert: "Artiste invalide." and return
+    end
+
     @quote_request = QuoteRequest.new(quote_request_params)
     @quote_request.client = current_user.userable
+    @quote_request.artist = artist
 
     if @quote_request.save
       redirect_to_conversation(@quote_request)
@@ -25,16 +31,14 @@ class QuoteRequestsController < ApplicationController
   end
 
   def accept
-    if @quote_request.update(status: :accepted)
-      Booking.create!(
-        client: @quote_request.client,
-        artist: @quote_request.artist
-      )
-      MessageFeed.find_or_create_by(client: @quote_request.client, artist: @quote_request.artist)
-      redirect_to quote_requests_path, notice: "Demande acceptée et rendez-vous créé."
-    else
-      redirect_to quote_requests_path, alert: "Impossible d'accepter la demande."
+    ActiveRecord::Base.transaction do
+      @quote_request.update!(status: :accepted)
+      Booking.create!(client: @quote_request.client, artist: @quote_request.artist)
+      MessageFeed.find_or_create_by!(client: @quote_request.client, artist: @quote_request.artist)
     end
+    redirect_to quote_requests_path, notice: "Demande acceptée et rendez-vous créé."
+  rescue ActiveRecord::RecordInvalid
+    redirect_to quote_requests_path, alert: "Impossible d'accepter la demande."
   end
 
   def reject
@@ -48,11 +52,13 @@ class QuoteRequestsController < ApplicationController
   private
 
   def set_quote_request
-    @quote_request = QuoteRequest.find(params[:id])
+    @quote_request = current_user.userable.quote_requests.find_by(id: params[:id])
+    unless @quote_request
+      redirect_to quote_requests_path, alert: "Demande non trouvée ou accès refusé."
+    end
   end
 
   def authorize_artist!
-    # Sécurité : ne montrer que les demandes qui appartiennent à l'artiste connecté
     unless current_user.userable_type == "Artist"
       redirect_to root_path, alert: "Accès non autorisé."
     end
@@ -89,10 +95,10 @@ class QuoteRequestsController < ApplicationController
   def quote_summary(quote)
     <<~MSG
       ✏️ Nouvelle demande de devis :
-      • Style : #{quote.style}
+      • Style : #{quote.style.join(', ')}
       • Taille : #{quote.size}
-      • Couleur : #{quote.color}
-      • Zone : #{quote.body_zone}
+      • Couleur : #{quote.color.join(', ')}
+      • Zone : #{quote.body_zone.join(', ')}
       • Allergies : #{quote.allergies}
       • Commentaire : #{quote.comments}
     MSG
