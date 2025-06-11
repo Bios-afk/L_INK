5253c1576ea43ba4a3b64ee8593c52f8d6a6c4ab
FK <felix.korbendau@free.fr>
2025-06-11 13:07:32 +0200

    pastille ok

--- app/models/message.rb
+++ app/models/message.rb
@@ -21,5 +21,27 @@ class Message < ApplicationRecord
                         partial: "messages/message",
                         target: "messages",
                         locals: { message: self, now_user: user }
+
+    broadcast_append_to "navbar_stream_#{message_feed.client.user.id}",
+                        partial: "shared/pastille_notif",
+                        target: "pastille-notif"
+
+    broadcast_append_to "navbar_stream_#{message_feed.artist.user.id}",
+                        partial: "shared/pastille_notif",
+                        target: "pastille-notif"
+
+    other_messages = message_feed.messages.where.not(user_id: user_id)
+    all_read = other_messages.all?(&:read)
+
+    html = ApplicationController.render(
+      partial: "message_feeds/read_status_class",
+      locals: { feed: message_feed, all_read: all_read }
+    )
+
+    broadcast_replace_to(
+      "feed_#{message_feed.id}",
+      target: "card-status-#{message_feed.id}",
+      html: html
+    )
   end
 end
